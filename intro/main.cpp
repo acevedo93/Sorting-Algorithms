bedee215441845ba387dac335a8afa97a326e3f1
David Acevedo <dacevedojaramillo@gmail.com>
2023-01-03 18:08:07 -0500

    Initial Commit

---
+++ intro/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  intro
+//
+//  Created by David Acevedo on 3/01/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
