c99d2fd27ab7d303ff4b76aca32fe06c95896847
David Acevedo <dacevedojaramillo@gmail.com>
2023-01-04 19:32:18 -0500

    bubble sort

--- intro/main.cpp
+++ intro/main.cpp
@@ -1,14 +1,25 @@
-//
-//  main.cpp
-//  intro
-//
-//  Created by David Acevedo on 3/01/23.
-//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    int values [] = {16,23,45,12};
+    int sizeArrray =  sizeof(values) / sizeof(int);
+    for (int i = 0; i < sizeArrray - 1; i++) {
+        for( int j = i+1; j < sizeArrray - i - 1; j++) {
+            if (values[i] > values[j]){
+                int temporalValue = values[j];
+                values[j] = values[i];
+                values[i] = temporalValue;
+            }
+        }
+    }
+    
+    for (int i = 0; i <= sizeArrray; i++) {
+        cout << values[i]<< endl;
+    }
+    
     return 0;
 }
+
